c8671ced4fe4411f40b99ea03c7f9fa157635efd
John_xm <hezhiqiang.a@gmail.com>
2019-03-29 20:09:36 -0700

    add tcprecv.c

---
+++ testOOB/tcprecv.c
@@ -0,0 +1,39 @@
+#include "unp.h"
+
+//
+// Created by john on 3/29/19.
+//
+int main(int argc,char **argv){
+    int listenfd,connfd,n;
+    char buf[100];
+    fd_set rset,xset;
+    if(argc==2)
+        listenfd = Tcp_listen(NULL,argv[1],NULL);
+    else if(argc==3)
+        listenfd = Tcp_listen(argv[1],argv[2],NULL);
+    else
+        err_quit("usage:tcprecv [<host>] [port#]");
+    connfd = Accept(listenfd,NULL,NULL);
+
+    FD_ZERO(rset);
+    FD_ZERO(xset);
+    for(;;){
+        FD_SET(connfd,&rset);
+        FD_SET(connfd,&xset);
+        Selcet(connfd+1,&rset,&wset,NULL);
+        if(FD_ISSET(connfd,&xset)){
+            n=Recv(connfd,buf,sizeof(buf)-1,MSG_OOB);
+            buf[n]=0;
+            printf("read %d OOB byte: %s\n",n,buf);
+        }
+        if(FD_ISSET(connfd,&rset)){
+            if((n=Read(connfd,buf,sizeof(buf)-1))==0){
+                printf("recvied EOF\n");
+                exit(0);
+            }
+            buf[0];
+            printf("read %d bytes: %s\n",n,buf);
+        }
+    }
+
+}
